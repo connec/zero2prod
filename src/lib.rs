6db4e6f78d77507bce74244e16bd133366768b03
Chris Connelly <chris.connelly@savvi.co>
2022-04-03 18:36:34 +0100

    feat: stub `POST /subscriptions` endpoint
    This introduces `serde`, letting us use axum's `Form` extractor to
    trivially parse the form data into a struct. We're not actually doing
    anything with the data yet, though.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use std::net::SocketAddr;
 
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{
+    extract::Form,
+    http::StatusCode,
+    routing::{get, post},
+    Router,
+};
 
 pub type Server =
     axum::Server<hyper::server::conn::AddrIncoming, axum::routing::IntoMakeService<Router>>;
@@ -8,11 +13,23 @@ pub type Server =
 pub type Result = hyper::Result<()>;
 
 pub fn bind(addr: &SocketAddr) -> Server {
-    let app = Router::new().route("/health", get(health));
+    let app = Router::new()
+        .route("/health", get(health))
+        .route("/subscriptions", post(subscribe));
 
     axum::Server::bind(addr).serve(app.into_make_service())
 }
 
 async fn health() -> StatusCode {
     StatusCode::NO_CONTENT
+}
+
+#[derive(serde::Deserialize)]
+struct Subscriber {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(Form(form): Form<Subscriber>) -> StatusCode {
+    StatusCode::OK
 }
--- tests/health.rs
+++ tests/health.rs
@@ -6,13 +6,57 @@ async fn health_works() {
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!("http://{}/health", addr))
+        .get(format!("http://{addr}/health"))
         .send()
         .await
         .expect("failed to execute request");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let addr = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("http://{addr}/subscriptions"))
+        .header("content-type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let addr = spawn_app().await;
+    let client = reqwest::Client::new();
+    let bodies = vec![
+        ("missing the email", "name=le%20guin"),
+        ("missing the name", "email=ursula_le_guin%40gmail.com"),
+        ("missing both name and email", ""),
+    ];
+
+    for (problem, body) in bodies {
+        let response = client
+            .post(format!("http://{addr}/subscriptions"))
+            .header("content-type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("failed to execute request");
+
+        assert_eq!(
+            422,
+            response.status().as_u16(),
+            "did not get 422 Unprocessable Entity when the payload was {problem}"
+        );
+    }
 }
 
 async fn spawn_app() -> SocketAddr {
