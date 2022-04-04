c24bfe59692405be5250f8dc6c2a5c3127f2ecec
Chris Connelly <chris.connelly@savvi.co>
2022-04-04 18:49:57 +0100

    chore: add `Dockerfile` and config necessary to build/run it
    The `Dockerfile` relies on `sqlx`'s offline mode in order to build
    without database access. This compels us to add a CI step to check that
    the `sqlx-data.json` is up-to-date. An `address` config has been added
    so that the docker container can bind to `0.0.0.0` rather than the
    default `127.0.0.1`. The `Dockerfile` uses `cargo-chef` to ensure
    dependency builds are cached.
    
    In a departure from the source material, we're using alpine from the
    get-go (10MB image), and using a whitelist-style `.dockerignore`.

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,15 @@
+use std::net::IpAddr;
+
 use sqlx::postgres::PgConnectOptions;
 
 #[derive(serde::Deserialize)]
 pub struct Config {
+    #[serde(default, deserialize_with = "ip_addr_from_str")]
+    pub address: Option<IpAddr>,
+
     #[serde(default)]
     pub port: Option<u16>,
+
     #[serde(rename = "database_url", deserialize_with = "database_url_from_str")]
     pub database: PgConnectOptions,
 }
@@ -12,6 +18,16 @@ impl Config {
     pub fn from_env() -> Result<Self, envy::Error> {
         envy::from_env()
     }
+}
+
+fn ip_addr_from_str<'de, D>(deserializer: D) -> Result<Option<IpAddr>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let ip_addr: Option<String> = serde::Deserialize::deserialize(deserializer)?;
+    ip_addr
+        .map(|ip_addr| ip_addr.parse().map_err(serde::de::Error::custom))
+        .transpose()
 }
 
 fn database_url_from_str<'de, D>(deserializer: D) -> Result<PgConnectOptions, D::Error>
