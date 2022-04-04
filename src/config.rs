ffd02ff73f7d097ee1249e3d4182920734a1b692
Chris Connelly <chris.connelly@savvi.co>
2022-04-04 23:13:24 +0100

    refactor: add convenient getters to `Config`
    The fields of `Config` are now all private, and instead some convenient
    getters can be used to obtain the configuration.

--- src/config.rs
+++ src/config.rs
@@ -1,22 +1,40 @@
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use sqlx::postgres::PgConnectOptions;
+
+const DEFAULT_ADDRESS: IpAddr = IpAddr::V4(Ipv4Addr::LOCALHOST);
+const DEFAULT_PORT: u16 = 8000;
 
 #[derive(serde::Deserialize)]
 pub struct Config {
     #[serde(default, deserialize_with = "ip_addr_from_str")]
-    pub address: Option<IpAddr>,
+    address: Option<IpAddr>,
 
     #[serde(default)]
-    pub port: Option<u16>,
+    port: Option<u16>,
 
     #[serde(rename = "database_url", deserialize_with = "database_url_from_str")]
-    pub database: PgConnectOptions,
+    database: PgConnectOptions,
 }
 
 impl Config {
     pub fn from_env() -> Result<Self, envy::Error> {
         envy::from_env()
+    }
+
+    pub fn addr(&self) -> SocketAddr {
+        SocketAddr::from((
+            self.address.unwrap_or(DEFAULT_ADDRESS),
+            self.port.unwrap_or(DEFAULT_PORT),
+        ))
+    }
+
+    pub fn database_options(&self) -> PgConnectOptions {
+        self.database.clone()
+    }
+
+    pub fn database_options_with_database(&self, name: &str) -> PgConnectOptions {
+        self.database.clone().database(name)
     }
 }
 
