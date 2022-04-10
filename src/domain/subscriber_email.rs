75725d175b803d0b98950e1a3e480d96c386e226
Chris Connelly <chris.connelly@savvi.co>
2022-04-10 13:07:37 +0100

    refactor: add `ConfigBuilder` to avoid stringly-typed config
    This rather egregious refactor of the `config` module allows for base
    configuration to be specified in a fully-typed way via a new
    `ConfigBuilder` (as seen in `tests/api/helpers.rs`). Additionally, now
    that `Config` is entirely read internally, most of the getters have been
    removed in favour of using `pub(crate)` fields meaning values can be
    moved to the `App` on construction, rather than cloning.

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,12 +1,18 @@
+use std::borrow::Cow;
+
 use super::Error;
 
 #[derive(Clone, Debug)]
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
-    pub(crate) fn parse(s: String) -> Result<Self, Error> {
-        if validator::validate_email(&s) {
-            Ok(Self(s))
+    pub(crate) fn parse<'s, S>(s: S) -> Result<Self, Error>
+    where
+        S: Into<Cow<'s, str>>,
+    {
+        let s = s.into();
+        if validator::validate_email(s.as_ref()) {
+            Ok(Self(s.into_owned()))
         } else {
             Err(Error(format!("{} is not a valid email", s)))
         }
@@ -16,6 +22,14 @@ impl SubscriberEmail {
 impl AsRef<str> for SubscriberEmail {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+impl std::str::FromStr for SubscriberEmail {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::parse(s).map_err(|error| error.to_string())
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::net::Ipv4Addr;
+
 use tracing::info;
 use zero2prod::App;
 
@@ -5,9 +7,12 @@ use zero2prod::App;
 async fn main() {
     zero2prod::telemetry::init(env!("CARGO_PKG_NAME"), std::io::stdout);
 
-    let config = zero2prod::Config::from_env().expect("failed to read configuration");
+    let config = zero2prod::Config::builder()
+        .address((Ipv4Addr::LOCALHOST, 8000).into())
+        .build()
+        .expect("failed to read configuration");
 
-    let app = App::new(&config);
+    let app = App::new(config);
     let server = app.serve().await.expect("failed to serve app");
 
     info!("Listening on {}", server.local_addr());
